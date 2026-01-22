11dc19162dc0c667c1b8037ce4405d2d67c3767e
Tushya Gupta <tushyagupta81@gmail.com>
2026-01-22 20:56:52 +0530

    fix to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,9 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tushyagupta81.github.io/terminal-portfolio',
+  site: 'https://tushyagupta81.github.io',
   // Only use base path in production (GitHub Pages)
-  base: process.env.NODE_ENV === 'production' ? '/astro-theme-terminal' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/terminal-portfolio' : '/',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
